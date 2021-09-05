6c1062793c3fd6d9ffbc5b4ce3a0f01b5e2c69ee
Islam Abdymazhit <islam.abdymazhit@gmail.com>
2021-09-05 19:11:40 +0300

    Добавлен файл конфигурации

--- src/main/java/net/abdymazhit/mthd/MTHD.java
+++ src/main/java/net/abdymazhit/mthd/MTHD.java
@@ -1,8 +1,67 @@
 package net.abdymazhit.mthd;
 
+import com.google.gson.Gson;
+import net.abdymazhit.mthd.customs.Config;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+
+/**
+ * Главный класс, отвечает за инициализацию бота
+ *
+ * @version   05.09.2021
+ * @author    Islam Abdymazhit
+ */
 public class MTHD {
 
-    public static void main(String[] args) {
+    /** Объект главного класса */
+    private static MTHD instance;
 
+    /** Файл конфигурации */
+    public Config config;
+
+    /**
+     * Создает бота
+     * @throws IOException Ошибка чтения файла конфигурации
+     */
+    public static void main(String[] args) throws IOException {
+        new MTHD();
+    }
+
+    /**
+     * Инициализирует бота
+     * @throws IOException Ошибка чтения файла конфигурации
+     */
+    public MTHD() throws IOException {
+        instance = this;
+        config = getConfig();
+    }
+
+    /**
+     * Получает файл конфигурации
+     * @return Файл конфигурации
+     * @throws IOException Ошибка чтения файла конфигурации
+     */
+    private Config getConfig() throws IOException {
+        Gson gson = new Gson();
+        File configFile = new File("config.json");
+
+        Config config;
+        if(!configFile.exists()) {
+            config = new Config();
+            Writer writer = Files.newBufferedWriter(configFile.toPath());
+            gson.toJson(config, writer);
+            writer.close();
+            System.exit(0);
+        } else {
+            Reader reader = Files.newBufferedReader(configFile.toPath());
+            config = gson.fromJson(reader, Config.class);
+            reader.close();
+        }
+
+        return config;
     }
 }
---
+++ src/main/java/net/abdymazhit/mthd/customs/Config.java
@@ -0,0 +1,16 @@
+package net.abdymazhit.mthd.customs;
+
+/**
+ * Представляет собой файл конфигурации
+ *
+ * @version   05.09.2021
+ * @author    Islam Abdymazhit
+ */
+public class Config {
+
+    /** Токен бота */
+    public String token = "BOT-TOKEN";
+
+    /** Токен VimeWorld.ru Public API */
+    public String vimeApiToken  = "VIMEWORLD_API_TOKEN";
+}
